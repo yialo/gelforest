84a373f03634d6b6c4a3696a7945c84f116c17c3
Aleksei Arro <alexey.arro@waytostart.ru>
2019-01-21 18:53:38 +0300

    Basic header-menu mechanics added

--- app/js/header-menu.js
+++ app/js/header-menu.js
@@ -17,9 +17,7 @@ const servicesData = [
 ];
 
 const pageHeader = document.querySelector('.js-header-menu');
-const areasMenuButton = pageHeader.querySelector(
-  '.js-header-menu__button',
-);
+const areasMenuButton = pageHeader.querySelector('.js-header-menu__button');
 
 pageHeader.classList.remove('no-js');
 pageHeader.classList.add('is-hidden');
@@ -31,6 +29,30 @@ areasMenuButton.addEventListener('click', () => {
 const servicesBlockTemplate = document
   .querySelector('#services-template')
   .content.querySelector('.services');
+
+const areasList = pageHeader.querySelector('.areas__list');
+const areasItemJeeping = areasList.querySelector('.areas__item--jeeping');
+
+areasItemJeeping.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  const servicesItemJeeping = servicesBlockTemplate.cloneNode(true);
+
+  const servicesList = servicesItemJeeping.querySelector('.services__list');
+  const servicesHeading = servicesItemJeeping.querySelector(
+    '.services__heading',
+  );
+  const servicesTitle = servicesItemJeeping.querySelector('.services__title');
+  const servicesPrice = servicesItemJeeping.querySelector('.services__price');
+  const areasListHeight = getComputedStyle(areasList).height;
+
+  servicesList.style.minHeight = areasListHeight;
+  servicesHeading.textContent = areasData[0];
+  servicesTitle.textContent = servicesData[0][0];
+  servicesPrice.textContent = servicesData[0][1];
+
+  pageHeader.appendChild(servicesItemJeeping);
+});
 
 /*
 const servicesScheduleElementTemplate = document
