b6cd0ad60cd5abdfe455309ed441fa13f90b5eeb
Aleksei Arro <alexey.arro@waytostart.ru>
2019-01-22 12:20:27 +0300

    Services menu logics added

--- app/js/header-menu.js
+++ app/js/header-menu.js
@@ -1,5 +1,27 @@
 'use strict';
 
+const pageHeader = document.querySelector('.js-header-menu');
+pageHeader.classList.remove('no-js');
+pageHeader.classList.add('is-hidden');
+
+const areasMenuButton = pageHeader.querySelector('.js-header-menu__button');
+areasMenuButton.addEventListener('click', () => {
+  pageHeader.classList.toggle('is-hidden');
+});
+window.addEventListener('keydown', (evt) => {
+  if (evt.keyCode === 27 && !pageHeader.classList.contains('is-hidden')) {
+    pageHeader.classList.add('is-hidden');
+  }
+});
+
+const pageHeaderLogo = pageHeader.querySelector('.page-header__logo');
+const areas = pageHeader.querySelector('.js-header-menu__container');
+const areasNest = areas.querySelector('.areas__nest');
+const pageHeaderLogoStyles = getComputedStyle(pageHeaderLogo);
+const pageHeaderLogoHeight = pageHeaderLogoStyles.height;
+const pageHeaderLogoMarginBottom  = pageHeaderLogoStyles.marginBottom;
+areasNest.style.top = `calc(${pageHeaderLogoHeight} * -1 - ${pageHeaderLogoMarginBottom})`;
+
 const areasData = [
   'Джиппинг',
   'Яхты',
@@ -16,35 +38,29 @@ const servicesData = [
   ['Ночной Геленджик', 1000],
 ];
 
-const pageHeader = document.querySelector('.js-header-menu');
-const areasMenuButton = pageHeader.querySelector('.js-header-menu__button');
-
-pageHeader.classList.remove('no-js');
-pageHeader.classList.add('is-hidden');
-
-areasMenuButton.addEventListener('click', () => {
-  pageHeader.classList.toggle('is-hidden');
-});
-
 const servicesBlockTemplate = document
   .querySelector('#services-template')
   .content.querySelector('.services');
 
-const areasList = pageHeader.querySelector('.areas__list');
+const areasList = areas.querySelector('.areas__list');
 const areasItems = areasList.querySelectorAll('.areas__item');
 
-const areasLinkClick = function areasLinkClickHandler(areasElement) {
-  areasElement.addEventListener('click', (evt) => {
+// Обработка кликов по пунктам главного меню
+
+const areasLinkClick = function areasLinkClickHandler(areasItemNumber) {
+  areasItems[areasItemNumber].addEventListener('click', (evt) => {
     evt.preventDefault();
     const services = servicesBlockTemplate.cloneNode(true);
-    const servicesHeading = services.querySelector(
+    const servicesReturnLink = services.querySelector('.services__return-link');
+    const servicesHeading = servicesReturnLink.querySelector(
       '.services__heading',
     );
-    servicesHeading.textContent = areasData[0];
+    servicesHeading.textContent = areasData[areasItemNumber];
 
     const servicesList = services.querySelector('.services__list');
     const areasListHeight = getComputedStyle(areasList).height;
     servicesList.style.minHeight = areasListHeight;
+
     const servicesItems = servicesList.querySelectorAll('.services__item');
 
     for (let i = 0; i < servicesItems.length; i += 1) {
@@ -54,10 +70,15 @@ const areasLinkClick = function areasLinkClickHandler(areasElement) {
       servicesPrice.textContent = servicesData[i][1];
     }
 
-    pageHeader.appendChild(services);
+    areasNest.appendChild(services);
+
+    servicesReturnLink.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      areasNest.removeChild(services);
+    });
   });
 };
 
 for (let i = 0; i < areasItems.length; i += 1) {
-  areasLinkClick(areasItems[i]);
+  areasLinkClick(i);
 }
