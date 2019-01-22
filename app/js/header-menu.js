1a4887e23d2efdaa3034fbba67f571fb21fc8ef0
Aleksei Arro <alexey.arro@waytostart.ru>
2019-01-22 11:00:55 +0300

    services menu generating added

--- app/js/header-menu.js
+++ app/js/header-menu.js
@@ -31,47 +31,33 @@ const servicesBlockTemplate = document
   .content.querySelector('.services');
 
 const areasList = pageHeader.querySelector('.areas__list');
-const areasItemJeeping = areasList.querySelector('.areas__item--jeeping');
+const areasItems = areasList.querySelectorAll('.areas__item');
 
-areasItemJeeping.addEventListener('click', (evt) => {
-  evt.preventDefault();
+const areasLinkClick = function areasLinkClickHandler(areasElement) {
+  areasElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    const services = servicesBlockTemplate.cloneNode(true);
+    const servicesHeading = services.querySelector(
+      '.services__heading',
+    );
+    servicesHeading.textContent = areasData[0];
 
-  const servicesItemJeeping = servicesBlockTemplate.cloneNode(true);
+    const servicesList = services.querySelector('.services__list');
+    const areasListHeight = getComputedStyle(areasList).height;
+    servicesList.style.minHeight = areasListHeight;
+    const servicesItems = servicesList.querySelectorAll('.services__item');
 
-  const servicesList = servicesItemJeeping.querySelector('.services__list');
-  const servicesHeading = servicesItemJeeping.querySelector(
-    '.services__heading',
-  );
-  const servicesTitle = servicesItemJeeping.querySelector('.services__title');
-  const servicesPrice = servicesItemJeeping.querySelector('.services__price');
-  const areasListHeight = getComputedStyle(areasList).height;
+    for (let i = 0; i < servicesItems.length; i += 1) {
+      const servicesTitle = servicesItems[i].querySelector('.services__title');
+      const servicesPrice = servicesItems[i].querySelector('.services__price');
+      servicesTitle.textContent = servicesData[i][0];
+      servicesPrice.textContent = servicesData[i][1];
+    }
 
-  servicesList.style.minHeight = areasListHeight;
-  servicesHeading.textContent = areasData[0];
-  servicesTitle.textContent = servicesData[0][0];
-  servicesPrice.textContent = servicesData[0][1];
+    pageHeader.appendChild(services);
+  });
+};
 
-  pageHeader.appendChild(servicesItemJeeping);
-});
-
-/*
-const servicesScheduleElementTemplate = document
-.querySelector('#service-schedule-template');
-*/
-
-// areasData.forEach((arrValue, arrIndex) => {
-//   const servicesElement = servicesElementTemplate.cloneNode(true);
-//   const servicesItemElements = servicesElement.querySelectorAll(
-//     '.services__item',
-//   );
-
-//   servicesData.forEach((value, index) => {
-//     const [title, price] = value;
-//     servicesItemElements[index].querySelector('.services__title')
-//       .textContent = title;
-//     servicesItemElements[index].querySelector('.services__price')
-//       .textContent = price;
-//   });
-
-//   // areasItemContentElements[arrIndex].appendChild(servicesElement);
-// });
+for (let i = 0; i < areasItems.length; i += 1) {
+  areasLinkClick(areasItems[i]);
+}
